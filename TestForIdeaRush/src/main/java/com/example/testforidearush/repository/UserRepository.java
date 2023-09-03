3bc3d3dd6e680beec1ef33e9698ec06001cb2171
ironprayer <ironprayer1208@gmail.com>
2023-09-03 23:31:52 +0900

    feat : testForIdea에서 필요 없는 엔티티 삭제

--- TestForIdeaRush/src/main/java/com/example/testforidearush/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.testforidearush.repository;
-
-import com.example.testforidearush.entity.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<Users, Long> {
-}
