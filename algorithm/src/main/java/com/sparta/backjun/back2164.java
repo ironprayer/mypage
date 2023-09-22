6849b645da9c7d0b4b6d59f15b5c697533f7f64e
ironprayer <ironprayer1208@gmail.com>
2023-09-22 22:41:18 +0900

    백준 2164번 풀기

---
+++ algorithm/src/main/java/com/sparta/backjun/back2164.java
@@ -0,0 +1,30 @@
+package com.sparta.backjun;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class back2164 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        Queue queue = new ArrayDeque();
+
+        for(int i = 1; i <= N; i++){
+            queue.add(i);
+        }
+
+        while(queue.size() != 1){
+            queue.poll();
+
+            int safeValue = (Integer) queue.poll();
+            queue.add(safeValue);
+        }
+
+        System.out.println(queue.poll());
+    }
+}
